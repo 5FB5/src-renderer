36d711cf160a51ab5d4e2e97f788663a707661e3
vzubko <vecktor911@gmail.com>
2023-09-07 12:07:24 +0300

    Init opengl

--- main.cpp
+++ main.cpp
@@ -1,6 +1,53 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+static void glfwError(int id, const char* desc)
+{
+    std::cout << "[GLFW Error]: " << desc << std::endl;
+}
+
+int main()
+{
+    glfwSetErrorCallback(glfwError);
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    GLFWwindow *mainWindow = glfwCreateWindow(600, 600, "src_renderer", nullptr, nullptr);
+
+    if (mainWindow == nullptr)
+    {
+        std::cout << "[GLFW Error]: Failed to create window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(mainWindow);
+
+    glewExperimental = GL_TRUE;
+
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "[GLEW Error]: Failed to init GLEW" << std::endl;
+        return -1;
+    }
+
+    int width, height;
+
+    glfwGetFramebufferSize(mainWindow, &width, &height);
+
+    glViewport(0, 0, width, height);
+
+    while(!glfwWindowShouldClose(mainWindow))
+    {
+        glfwPollEvents();
+        glfwSwapBuffers(mainWindow);
+    }
+
+    glfwTerminate();
+
     return 0;
 }
