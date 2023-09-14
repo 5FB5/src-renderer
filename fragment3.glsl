6d28b17349ea3e729197676d994cbeb618f9c1ba
vzubko <vecktor911@gmail.com>
2023-09-14 13:48:13 +0300

    load and mix textures

--- fragment3.glsl
+++ fragment3.glsl
@@ -1,8 +1,14 @@
 #version 330 core
 
+in vec2 TexCoord;
+in vec3 outColor;
 out vec4 color;
+
+uniform sampler2D baseTexture;
+uniform sampler2D secondTexture;
+uniform float mixValue;
 
 void main()
 {
-    color = vec4(1.0f, 1.0f, 0.3f, 1.0f);
+    color = mix(texture(baseTexture, TexCoord), texture(secondTexture, TexCoord), mixValue);
 }
--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <SOIL/SOIL.h>
 #include <iostream>
 #include <math.h>
 
@@ -69,15 +70,32 @@ int main()
             0.8, -0.5, 0.0
     };
 
+
     GLfloat vertices3[] = {
-            -0.8f, -0.8f, 0.0f,
-            0.0f, 0.8f, 0.0f,
-            0.8, -0.8f, 0.0f
+            0.4f, 0.4f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,// Top Right
+            0.4f, -0.4f, 0.0f, 0.f, 1.0f, 0.0f, 1.0f, 0.0f,// Bottom Right
+            -0.3f, -0.4f, 0.0f,0.0f, 0.0f, 1.0f, 0.0f, 0.0f,// Bottom Left
+            -0.3f, 0.4f, 0.0f, 0.0f, 0.0f ,0.0f, 0.0f, 1.0f// Top Left
     };
 
-    GLuint VAOs[3], VBOs[3];
+    GLuint indices[] = {
+            0, 1, 3,
+            1, 2, 3
+    };
+
+    int texW, texH;
+    unsigned char *image = SOIL_load_image("test.jpg", &texW, &texH, 0, SOIL_LOAD_RGB);
+
+    int tex2W, tex2H;
+    unsigned char *image2 = SOIL_load_image("test2.jpg", &tex2W,&tex2H, 0, SOIL_LOAD_RGB);
+
+    GLuint VAOs[3], VBOs[3], ibo, texture, texture2;
+
     glGenVertexArrays(3, VAOs);
     glGenBuffers(3, VBOs);
+    glGenBuffers(1, &ibo);
+    glGenTextures(1, &texture);
+    glGenTextures(1, &texture2);
 
     // First triangle
     glBindVertexArray(VAOs[0]);
@@ -102,15 +120,44 @@ int main()
 
     glBindVertexArray(0);
 
-    // Third triangle
+    // Rectangle
     glBindVertexArray(VAOs[2]);
     glBindBuffer(GL_ARRAY_BUFFER, VBOs[2]);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices3), vertices3, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), static_cast<GLvoid *>(0));
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // Generate textures
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, texW, texH, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // Set texture filtering
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);  // NOTE the GL_NEAREST Here!
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);  // NOTE the GL_NEAREST Here!
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, tex2W, tex2H, 0, GL_RGB, GL_UNSIGNED_BYTE, image2);
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), static_cast<GLvoid*>(0));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), reinterpret_cast<GLvoid*>(3 * sizeof(GLfloat)));
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), reinterpret_cast<GLvoid*>(6 * sizeof(GLfloat)));
+
     glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+    glEnableVertexAttribArray(2);
 
     glBindVertexArray(0);
+
+    SOIL_free_image_data(image);
+    SOIL_free_image_data(image2);
 
     while(!glfwWindowShouldClose(mainWindow))
     {
@@ -140,8 +187,23 @@ int main()
 
         shader3.use();
 
+        GLfloat texMixValue = std::abs((sin(timeValue * 3)));
+        GLint mixValueLocation = glGetUniformLocation(shader3.program, "mixValue");
+        glUniform1f(mixValueLocation, texMixValue);
+
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glUniform1i(glGetUniformLocation(shader3.program, "baseTexture"), 0);
+
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+        glUniform1i(glGetUniformLocation(shader3.program, "secondTexture"), 1);
+
         glBindVertexArray(VAOs[2]);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
         glBindVertexArray(0);
 
         glfwPollEvents();
