6bf198fb13eb4527a892517739f36a9250fc2a50
vzubko <vecktor911@gmail.com>
2023-09-27 20:45:29 +0300

    cmakelists refactor

---
+++ libs/valve/bsp.cpp
@@ -0,0 +1,8 @@
+//
+// Created by valera on 27.09.23.
+//
+
+#include "bsp.h"
+
+namespace vbsp {
+} // vbsp
---
+++ libs/valve/bsp.h
@@ -0,0 +1,16 @@
+//
+// Created by valera on 27.09.23.
+//
+
+#ifndef SRC_RENDERER_BSP_H
+#define SRC_RENDERER_BSP_H
+
+namespace vbsp {
+
+    class bsp {
+
+    };
+
+} // vbsp
+
+#endif //SRC_RENDERER_BSP_H
